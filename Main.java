c14ae562211c93c65b44237e5120b891641a34fc
bahassi123 <bahassmm@mail.uc.edu>
2021-02-03 04:16:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class Main {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+System.out.println("Enter a number to count down or up from: "); 
+  }
+}
